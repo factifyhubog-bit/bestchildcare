3dab02923515a0d711c1079bed495b383c8f1e49
factifyhubog-bit <factifyhubog@gmail.com>
2026-01-05 13:04:10 +0530

    Update script.js
    Signed-off-by: factifyhubog-bit <factifyhubog@gmail.com>

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 4000);
+
 function shareSite() {
   if (navigator.share) {
     navigator.share({
-      title: 'BestChildCare',
-      text: 'Help save a child’s life by supporting medical treatment.',
+      title: "BestChildCare",
+      text: "Help save a child’s life by supporting medical treatment.",
       url: window.location.href
     });
   } else {
-    alert("Please copy and share this link:\n" + window.location.href);
+    alert("Please share this link:\n" + window.location.href);
   }
 }
